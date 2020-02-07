7300eeb95b06d1df90e6f73df5db87fb34bdec21
puttappa.harshavardhan@gmail.com <puttappa.harshavardhan@gmail.com>
2020-02-07 13:16:07 +0530

    Updates

---
+++ precache-manifest.fed1679e81c5040dc4754cf0745ee515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa40beedcbf7c70ee67a37260cebdd29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a8a26ba5491166fac8ad",
+    "url": "/monsters-rolodex/static/css/main.c842c75e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8a26ba5491166fac8ad",
+    "url": "/monsters-rolodex/static/js/main.85e4e0fd.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
