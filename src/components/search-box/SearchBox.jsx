f8ac3ec323aa28a0abbf48464e9e2bcadcd6b8a2
Harshavardhan Puttappa <puttappa.harshavardhan@gmail.com>
2020-02-07 12:24:09 +0530

    adding files to the github repository

---
+++ src/components/search-box/SearchBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./search-box.styles.css";
+
+export default ({ placeholder, handleChange }) => {
+  return (
+    <input
+      className="search"
+      type="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+    />
+  );
+};
