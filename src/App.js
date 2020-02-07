dca1c4c8db6725b58e2efba1d8516d7d445a4709
puttappa.harshavardhan@gmail.com <puttappa.harshavardhan@gmail.com>
2020-02-07 13:56:51 +0530

    final

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     searchFieldValue: ""
   };
   componentDidMount() {
-    fetch("http://jsonplaceholder.typicode.com/users/")
+    fetch("https://jsonplaceholder.typicode.com/users/")
       .then(res => res.json())
       .then(users => this.setState({ monsters: users }));
   }
